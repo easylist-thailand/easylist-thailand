08e58b18e4eb00a27db94caf933c71195d306af5
Saran Tanpituckpong <sarunta@gmail.com>
2018-10-05 23:43:42 +0700

    :construction_worker: Add Travis CI config & scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy_key
+ssh-add .travis/deploy_key
+git remote add deploy git@github.com:easylist-thailand/easylist-thailand.git
+git add subscription/easylist-thailand.txt
+git commit -m ":bento: Auto-generate subscription from Travis CI (Build $TRAVIS_BUILD_ID)" -m "Travis CI log: $TRAVIS_BUILD_WEB_URL"
+git push deploy
