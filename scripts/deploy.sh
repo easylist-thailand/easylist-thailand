089f0d4f29524faa96431e8729555b412d287904
Saran Tanpituckpong <sarunta@gmail.com>
2018-10-06 18:33:51 +0700

    :green_heart: Add pushing branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ ssh-add .travis/deploy_key
 git remote add deploy git@github.com:easylist-thailand/easylist-thailand.git
 git add subscription/easylist-thailand.txt
 git commit -m ":bento: Auto-generate subscription from Travis CI (Build $TRAVIS_BUILD_ID)" -m "Travis CI log: $TRAVIS_BUILD_WEB_URL"
-git push deploy
+git push deploy master
