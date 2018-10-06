c2689205e2ecd231b8a74890b7668d30c981cfee
Saran Tanpituckpong <sarunta@gmail.com>
2018-10-06 22:08:47 +0700

    :rotating_light: Fix linting errors

--- Rakefile
+++ Rakefile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'colorize'
 require './lib/easylist_generator'
 
--- lib/easylist_generator/version.rb
+++ lib/easylist_generator/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module EasylistGenerator
-  VERSION = '2.2.1'.freeze
+  VERSION = '2.2.1'
 end
