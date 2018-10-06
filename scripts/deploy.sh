ee98492703d02a319fcccbac5142425a97f5443a
Saran Tanpituckpong <sarunta@gmail.com>
2018-10-06 22:59:11 +0700

    :rocket: Add skip ci in auto-generated commit message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,5 +12,5 @@ git remote add deploy git@github.com:easylist-thailand/easylist-thailand.git
 # Push
 git checkout master
 git add subscription/easylist-thailand.txt
-git commit -m ":bento: Auto-generate subscription from Travis CI (Build $TRAVIS_BUILD_ID)" -m "Travis CI log: $TRAVIS_BUILD_WEB_URL"
+git commit -m ":bento: [skip ci] Auto-generate subscription from Travis CI (Build $TRAVIS_BUILD_ID)" -m "Travis CI log: $TRAVIS_BUILD_WEB_URL"
 git push -u deploy master
