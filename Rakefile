ab56f9b443dafca06e0a0eb64c3c84cac72beb69
Saran Tanpituckpong <4688092+gluons@users.noreply.github.com>
2020-03-07 20:57:06 +0700

    :recycle: Simplify methods usage

--- Rakefile
+++ Rakefile
@@ -9,22 +9,14 @@ task default: :generate
 
 desc 'Clean subscription file.'
 task :clean do
-  EasylistGenerator.clean('./subscription')
+  EasylistGenerator.clean
+
   puts '[Clean] Subscription file removed.'.colorize(:red)
 end
 
 desc 'Generate subscription file.'
 task generate: %i[clean] do
-  content = EasylistGenerator.concat('./categories/*.txt')
-  content = EasylistGenerator.head('./assets/header.txt', content)
-  content += EasylistGenerator.ublock
-
-  EasylistGenerator.ensure_dir('./subscription')
-  File.open('./subscription/easylist-thailand.txt', 'w') do |file|
-    file.puts(content)
-  end
-
-  EasylistGenerator.copy './categories/ublock.txt', './subscription/ublock.txt'
+  EasylistGenerator.generate
 
   puts '[Generate] Subscription file created.'.colorize(:green)
 end
--- lib/easylist_generator.rb
+++ lib/easylist_generator.rb
@@ -3,30 +3,51 @@
 require 'English'
 require 'fileutils'
 
-dir = File.expand_path(__dir__)
-Dir.glob("#{dir}/easylist_generator/*.rb").sort.each do |file|
+current_dir = File.expand_path __dir__
+Dir.glob("#{current_dir}/easylist_generator/*.rb").sort.each do |file|
   require file
 end
 
+SUBSCRIPTION_DIR = File.expand_path '../subscription/', __dir__
+SUBSCRIPTION_FILE_PATH = "#{SUBSCRIPTION_DIR}/easylist-thailand.txt"
+HEADER_FILE_PATH = File.expand_path '../assets/header.txt', __dir__
+CATEGORIES_DIR = File.expand_path '../categories/', __dir__
+UBLOCK_FILENAME = 'ublock.txt'
+UBLOCK_DIRECTIVE = <<~UBLOCK.strip
+  !#if ext_ublock
+  !#include #{UBLOCK_FILENAME}
+  !#endif
+UBLOCK
+
 # Easylist Generator
 module EasylistGenerator
-  def self.head(header_file_path, content)
+  ##
+  # Ensure subscription directory exist.
+  def self.ensure_dir
+    Dir.mkdir SUBSCRIPTION_DIR unless Dir.exist? SUBSCRIPTION_DIR
+  end
+
+  ##
+  # Get subscription header content.
+  def self.head
     now = Time.now
     version = now.strftime '%Y%m%d%H%M'
     last_modified = now.strftime '%d %b %Y %H:%M %z'
 
-    header = File.read(header_file_path)
-    header = format(header, version: version, last_modified: last_modified)
-    result = header + ($RS * 2)
-    result += content
+    header = File.read HEADER_FILE_PATH
+    header = format header, version: version, last_modified: last_modified
 
-    result
+    header
   end
 
-  def self.concat(src_glob)
+  ##
+  # Concat all filters from categories directory.
+  def self.concat
+    src_glob = "#{CATEGORIES_DIR}/*.txt"
+
     result = ''
 
-    Dir.glob(src_glob) do |file_path|
+    Dir.glob(src_glob).sort.each do |file_path|
       next if file_path.include? 'ublock'
 
       result += File.read(file_path) + $RS
@@ -35,23 +56,35 @@ module EasylistGenerator
     result
   end
 
+  ##
+  # Copy uBlock specific filters
+  # from categories directory to subscription directory.
   def self.ublock
-    <<~UBLOCK
-      !#if ext_ublock
-      !#include ublock.txt
-      !#endif
-    UBLOCK
+    src_file = "#{CATEGORIES_DIR}/#{UBLOCK_FILENAME}"
+    dest_file = "#{SUBSCRIPTION_DIR}/#{UBLOCK_FILENAME}"
+
+    FileUtils.copy src_file, dest_file
   end
 
-  def self.copy(src_file_path, dest_file_path)
-    FileUtils.cp src_file_path, dest_file_path
+  ##
+  # Generate subscription files.
+  def self.generate
+    content = head + ($RS * 2) + concat + UBLOCK_DIRECTIVE
+
+    ensure_dir
+
+    File.open(SUBSCRIPTION_FILE_PATH, 'w') do |file|
+      file.puts content
+    end
+
+    ublock
   end
 
-  def self.clean(dir)
-    FileUtils.rm_rf(dir)
+  ##
+  # Clean subscription directory.
+  def self.clean
+    FileUtils.rm_rf SUBSCRIPTION_DIR
   end
 
-  def self.ensure_dir(dir)
-    Dir.mkdir(dir) unless Dir.exist?(dir)
-  end
+  private_class_method :ensure_dir, :head, :concat, :ublock
 end
